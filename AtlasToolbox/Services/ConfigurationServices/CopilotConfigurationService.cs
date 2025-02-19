0ad388f6d4235e58e1d25a3bfac2d945776810af
TheyCreeper <thecrepermdrl@gmail.com>
2025-02-19 08:51:40 -0500

    Auto stash before merge of "theycreeper" and "origin/theycreeper"

--- AtlasToolbox/Services/ConfigurationServices/CopilotConfigurationService.cs
+++ AtlasToolbox/Services/ConfigurationServices/CopilotConfigurationService.cs
@@ -1,4 +1,4 @@
-﻿using AtlasToolbox.Stores;
+using AtlasToolbox.Stores;
 using AtlasToolbox.Utils;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -29,6 +29,7 @@ namespace AtlasToolbox.Services.ConfigurationServices
         public void Disable()
         {
             RegistryHelper.SetValue(ATLAS_STORE_KEY_NAME, STATE_VALUE_NAME, 0);
+            RegistryHelper.SetValue(ATLAS_STORE_KEY_NAME, "path", @$"{Environment.GetEnvironmentVariable("windir")}\AtlasDesktop\3. General Configuration\AI Features\Microsoft Copilot\Disable Microsoft Copilot (default).cmd");
             CommandPromptHelper.RunCommand(@$"{Environment.GetEnvironmentVariable("windir")}AtlasModules\Toolbox\Scripts\Copilot\DisableMicrosoftCopilot.cmd");
 
             _copilotConfigurationStore.CurrentSetting = IsEnabled();
@@ -37,6 +38,8 @@ namespace AtlasToolbox.Services.ConfigurationServices
         public void Enable()
         {
             RegistryHelper.SetValue(ATLAS_STORE_KEY_NAME, STATE_VALUE_NAME, 1);
+            RegistryHelper.SetValue(ATLAS_STORE_KEY_NAME, "path", @$"{Environment.GetEnvironmentVariable("windir")}\AtlasDesktop\3. General Configuration\AI Features\Microsoft Copilot\Enable Microsoft Copilot.cmd");
+
             CommandPromptHelper.RunCommand(@$"{Environment.GetEnvironmentVariable("windir")}AtlasModules\Toolbox\Scripts\Copilot\EnableMicrosoftCopilot.cmd");
 
             _copilotConfigurationStore.CurrentSetting = IsEnabled();
