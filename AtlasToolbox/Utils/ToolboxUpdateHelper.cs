ff40fa8aa5cebce57421d9fbb8e55d1a28ae9225
Creeper <thecreepermdrl@gmail.com>
2025-05-19 20:27:22 -0400

    fix: added a try-catch

--- AtlasToolbox/Utils/ToolboxUpdateHelper.cs
+++ AtlasToolbox/Utils/ToolboxUpdateHelper.cs
@@ -19,24 +19,30 @@ namespace AtlasToolbox.Utils
         public static string commandUpdate;
         public static bool CheckUpdates()
         {
-            // get the api result
-            string htmlContent = CommandPromptHelper.ReturnRunCommand("curl " + RELEASE_URL);
-            var result = JsonDocument.Parse(htmlContent);
-            string tagName = result.RootElement.GetProperty("tag_name").GetString();
-
-            // Format everything to compare 
-            int version = int.Parse(RegistryHelper.GetValue($@"HKLM\SOFTWARE\AtlasOS\Toolbox", "Version").ToString().Replace(".", ""));
-
-            if (int.Parse(tagName.Replace(".", "").Replace("v", "")) > version)
+            try
             {
-                // get the download link and create a temporary directory
-                string downloadUrl = result.RootElement.GetProperty("assets")[0].GetProperty("browser_download_url").GetString();
-                string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-                Directory.CreateDirectory(tempDirectory);
+                // get the api result
+                string htmlContent = CommandPromptHelper.ReturnRunCommand("curl " + RELEASE_URL);
+                var result = JsonDocument.Parse(htmlContent);
+                string tagName = result.RootElement.GetProperty("tag_name").GetString();
 
-                CommandPromptHelper.RunCommand($"cd {tempDirectory} && curl -LSs {downloadUrl} -O \"setup.exe\"");
-                commandUpdate = $"{tempDirectory}\\{downloadUrl.Split('/').Last()} /silent /install";
-                return true;
+                // Format everything to compare 
+                int version = int.Parse(RegistryHelper.GetValue($@"HKLM\SOFTWARE\AtlasOS\Toolbox", "Version").ToString().Replace(".", ""));
+
+                if (int.Parse(tagName.Replace(".", "").Replace("v", "")) > version)
+                {
+                    // get the download link and create a temporary directory
+                    string downloadUrl = result.RootElement.GetProperty("assets")[0].GetProperty("browser_download_url").GetString();
+                    string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                    Directory.CreateDirectory(tempDirectory);
+
+                    CommandPromptHelper.RunCommand($"cd {tempDirectory} && curl -LSs {downloadUrl} -O \"setup.exe\"");
+                    commandUpdate = $"{tempDirectory}\\{downloadUrl.Split('/').Last()} /silent /install";
+                    return true;
+                }
+            }catch
+            {
+                return false;
             }
             return false;
         }
